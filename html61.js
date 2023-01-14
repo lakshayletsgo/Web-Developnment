d194b34a8f41373a606f7be6b6f9b0398b590d0c
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-01-14 20:55:24 +0530

    html61 Sync and Async codes

---
+++ html61.js
@@ -0,0 +1,15 @@
+// Synchronous or Blocking Code
+// Isme Line by line execution hoga 
+// let text = fs.readFileSync("sample.txt","utf-8");
+
+
+// Asynchronous or Non-Blocking
+// Isme Line by line execution hoga not gurrantted
+// Callbacks Fires
+// let text = fs.readFile("sample.txt","utf-8",(err,data)=>{
+//     console.log(err,data);
+//     });
+
+
+// Async me voh jab tak apna kaam karega tab tak baaki aage wale run ho jayege
+// Abhi itna important nahi hai 
