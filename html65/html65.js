667550024398c7cc7bbc422d5d380e0125a1c143
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-01-16 18:09:37 +0530

    html65 Learning About Npm and its modules

---
+++ html65/html65.js
@@ -0,0 +1,10 @@
+console.log("This is Tutorial 65");
+//1.1.0
+//1.1.1 --> This means bug fixes
+// 1.2.0 --> This means new features are added
+// 2.0.0 --> This means old features are removed and new ones are added
+// Dependencies me voh packages honge jo ham download karege
+// Dev dependencies me voh packages honge jo hame sirf coding ke time chahiye deployment ke time nahi
+// Global likhne se voh library hamare system me install honge
+// Nodemon se agr ham kuch changes karege apne code me toh ham usse saath ke saath browser me dhek sakange
+// Sirf I likne se bhi install ho jayega
